2e8b8b46ccc7e71f8f5e86251e4598357d8a1332
David Hall <davidross.hall@mail.utoronto.ca>
2020-09-03 12:11:03 -0400

    Apparently webchem package doesn't work when compiling Rmarkdown documents. So writing plain r script.

---
+++ smilesAndCas.R
@@ -0,0 +1,31 @@
+library(webchem) 
+library(rcdk)
+
+CASRN <- c("73829-36-4",
+           "196859-54-8",
+           "171978-95-3",
+           "862374-87-6",
+           "1763-23-1",
+           "754-91-6")
+
+
+cids <- get_cid(CASRN) # get's the pubchem Compound IDs (cids)
+
+# Get properties listed on pubchem
+x <- pc_prop(cids$cid,
+             properties = "CanonicalSMILES",
+             )
+
+x$CAS <- CASRN
+
+smiles <- as.vector(x$CanonicalSMILES)
+
+m <- parse.smiles(smiles)
+dep <- get.depictor(width=300,height = 300,style='nob')
+copy.image.to.clipboard(m,dep)
+img <- view.image.2d(m,dep)
+
+plot(c(100, 300), c(100, 300), type = "n", xlab = "", ylab = "")
+rasterImage(img, 100, 100, 300, 300 )
+
+
